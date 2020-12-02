d112fe73ec53120e5ba93330506b7f69df319845
Paul Orlob <orlobpaul@gmail.com>
2020-12-02 19:16:06 +0100

    Added test for erroneous connection to device

--- pkg/sunspec/device_test.go
+++ pkg/sunspec/device_test.go
@@ -79,3 +79,10 @@ func TestConnect_Scan(t *testing.T) {
 		t.Fatalf("want %v, got %v", 1337, p)
 	}
 }
+
+func TestConnect_Err(t *testing.T) {
+	_, err := sunspec.Connect(1, "127.0.0.2:1234")
+	if err == nil {
+		t.Fatalf("expected error")
+	}
+}
